d15ebe306e02dbe4de74b9926dac7deb06dd434a
keitaro92 <nicolas.dalmolin@gmail.com>
2018-05-13 14:42:01 +0200

    enrich ConsoleEmitter

--- Monithor.ConsoleEmitter/Program.cs
+++ Monithor.ConsoleEmitter/Program.cs
@@ -31,7 +31,7 @@ namespace Monithor.ConsoleEmitter
             {
                 displayer.LogException("monithor emitter error occured =(", e);
             }
-
+            Random random = new Random();
             int i = 0;
             do
             {
@@ -41,13 +41,47 @@ namespace Monithor.ConsoleEmitter
                     await monithorEmitter.UpdateMetric(MessageLevel.Verbose, MessageType.Communication, "posX",
                         i.ToString());
                     displayer.LogTrace($"sent metric posX {i}");
+                    await monithorEmitter.UpdateMetric(MessageLevel.Verbose, MessageType.Communication, "posY",
+                        i.ToString());
+                    displayer.LogTrace($"sent metric posY {i}");
+                    await monithorEmitter.UpdateMetric(MessageLevel.Verbose, MessageType.Communication, "posZ",
+                        i.ToString());
+                    displayer.LogTrace($"sent metric posZ {i}");
+
+
+                    if (i % 7 == 0)
+                    {
+                        var criticity = random.Next(10);
+                        MessageLevel level;
+                        string message;
+
+                        if (criticity <= 1)
+                        {
+                            level = MessageLevel.Error;
+                            message = "An engine just crashed.";
+                        }   
+                        else if (criticity <= 4)
+                        {
+                            level = MessageLevel.Informational;
+                            message = "Target position reached.";
+                        }
+                        else
+                        {
+                            level = MessageLevel.Verbose;
+                            message = "Nothing detected in radar";
+                        }
+
+                        await monithorEmitter.Trace(level, MessageType.Communication, "trace", message, "");
+                        displayer.LogTrace($"sent trace {i}");
+                    }
+
                     i++;
                 }
                 catch (MonithorClientException e)
                 {
                     displayer.LogException("send trace failed", e);
                 }
-             
+
 
             } while (true);
         }
